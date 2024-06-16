d0cf3a93abacfa62d0d7cf17d4df2cc68900c5be
mayambacode <bokambma@sheridancollege.ca>
2024-06-15 23:08:51 -0400

    feat: Add form submission functionality to weather app

--- index.js
+++ index.js
@@ -5,3 +5,45 @@ const cityInput = document.querySelector('.cityInput');
 const card = document.querySelector('.card');
 const apiKey = "a1d0551adb2823e55b85af9bad4a32f4";
 
+weatherForm.addEventListener('submit',event =>{
+
+    //stops form from refreshing
+    event.preventDefault();
+
+    // access whatever text we put into city input field    
+    const city = cityInput.value;
+
+    // if there is a city name do something with
+    if(city){
+
+    }
+    else {
+        displayError("Please enter a city name");
+    }
+
+});
+
+//get data based on city
+async function getWeatherData(city) {
+
+}
+
+//show weather information in json format
+function displayWeatherInfo(data) {
+
+}
+
+// get weather emoji based on weather id
+function getWeatherEmoji(weatherId) {
+
+}
+
+// Show error message if weather data is not available
+function displayError(message){
+
+    // create a new paragraph element
+    const errorDisplay = document.createElement('p')
+    
+    errorDisplay.textContent = message;
+    errorDisplay.classList.add("errorDisplay");
+}
