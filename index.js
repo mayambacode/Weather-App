5b53fa28ae39366e406b2c4c0c3110f1b121d6bb
mayambacode <bokambma@sheridancollege.ca>
2024-06-15 22:55:25 -0400

    javascript code being written

---
+++ index.js
@@ -0,0 +1,7 @@
+// WEATHER APP
+
+const weatherForm = document.querySelector('.weatherForm');
+const cityInput = document.querySelector('.cityInput');
+const card = document.querySelector('.card');
+const apiKey = "a1d0551adb2823e55b85af9bad4a32f4";
+
